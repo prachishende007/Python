c6c9f6dd2030069e52444c30768ab43f0282bdb4
Prachi Shende <prachishende182@gmail.com>
2025-12-26 18:49:48 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,14 @@
+def avg():
+   a = int(input("Enter a number:"))
+   b = int(input("Enter a number:"))
+
+   average = (a+b)/2
+   print(average)
+
+avg()
+
+def greet(name, ending="Thank You"):
+    print(f"Good Day, {name}")
+    print(ending)
+
+greet("Prachi")
