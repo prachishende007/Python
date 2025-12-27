095eec7832f65b4a7bcd73a477b817b8e4e88494
Prachi Shende <prachishende182@gmail.com>
2025-12-27 17:15:07 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,26 @@
+# for i in range(1, 6, 2):
+#     print(i)
+
+l1 = ["Prachi", True, 17.00, 26]
+
+# for i in range(len(l1)):
+#     print(l1[i])
+
+for i in l1:
+    print(i)
+
+s = "Prachi"
+
+for i in s:
+    print(i)
+
+# i = 1
+# while(i < 6):
+#     print(i)
+#     i += 1
+
+
+# i = 0
+# while(1 < len(l1)):
+#     print(l1[i])
+#     i += 1
